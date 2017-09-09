5d384773c8fbce10337851e3bacae3a9899bf3df
MrF <fayazbasha974@gmail.com>
2017-09-10 01:11:53 +0530

    done

--- src/app/home/components/characteristics/characteristics.component.ts
+++ src/app/home/components/characteristics/characteristics.component.ts
@@ -10,50 +10,58 @@ import { SettingsService } from '../../services/settings/settings.service';
 export class CharacteristicsComponent implements OnInit {
 
   characteristics:any[];
+  currentCharacteristicId:number=0;
+  selectedCharacteristic:number;
 
   constructor(public settings:SettingsService) { 
     this.characteristics=[
       { id:"0",
         description:"vechile identification number",
         value:[
-          {charValu:"1231", valueDesc:"",pricing:"100",status:''},
-          {charValu:"8888", valueDesc:"",pricing:"600",status:''},
-          {charValu:"9999", valueDesc:"",pricing:"1000",status:''}
+          {charValu:"1231", valueDesc:"abcd",pricing:"100",status:''},
+          {charValu:"8888", valueDesc:"abc",pricing:"600",status:''},
+          {charValu:"9999", valueDesc:"abd",pricing:"1000",status:''}
         ]
       },
       {
         id:"2",
         description:"paint code",
         value:[
-          {charValu:"a3da", valueDesc:"",pricing:"",status:''},
-          {charValu:"", valueDesc:"",pricing:"",status:''}
+          {charValu:"a3da", valueDesc:"abcd",pricing:"",status:''},
+          {charValu:"678", valueDesc:"fvdg",pricing:"",status:''}
         ]
       },
       {
         id:"3",
         description:"door",
         value:[
-          {charValu:"", valueDesc:"",pricing:"",status:''}
         ]
       },
       {
         id:"4",
         description:"front options",
         value:[
-          {charValu:"", valueDesc:"",pricing:"",status:''}
+          {charValu:"234", valueDesc:"gyth",pricing:"",status:''}
         ]
       },
-      {id:"5",description:"interior options",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
-      {id:"0",description:"passenger side door",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
-      {id:"6",description:"driver side door",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
-      {id:"0",description:"interior lights",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},      
+      {id:"5",description:"interior options",value:[]},
+      {id:"0",description:"passenger side door",value:[{charValu:"8907", valueDesc:"fgtyh",pricing:"",status:''}]},
+      {id:"6",description:"driver side door",value:[]},
+      {id:"0",description:"interior lights",value:[{charValu:"0987", valueDesc:"hbgyt",pricing:"",status:''}]},      
     ]
   }
 
-  showVlaues(event,value){
-    console.log(value)
+  showVlaues(event,index){
+    this.currentCharacteristicId = index;
   }
-
+  selectCharacteristic(){
+    for(var i=0; i<this.characteristics[this.currentCharacteristicId].value.lenght;i++){
+        this.characteristics[this.currentCharacteristicId].value[i].status = '';
+    }
+    this.characteristics[this.currentCharacteristicId].value[this.selectedCharacteristic].status = 'selected';
+    console.log(this.characteristics);
+    this.selectedCharacteristic=null;
+  }
   ngOnInit() {
   }
 
