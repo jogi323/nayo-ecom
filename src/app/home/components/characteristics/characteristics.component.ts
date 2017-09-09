428b90050b1c7d6ad24b1dad323707eb40eed47f
ak <ashokona@gmail.com>
2017-09-09 22:04:33 +0530

    done

--- src/app/home/components/characteristics/characteristics.component.ts
+++ src/app/home/components/characteristics/characteristics.component.ts
@@ -13,15 +13,45 @@ export class CharacteristicsComponent implements OnInit {
 
   constructor(public settings:SettingsService) { 
     this.characteristics=[
-      {id:"0",description:"vechile identification number",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"2",description:"paint code",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"3",description:"door",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"4",description:"front options",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"5",description:"interior options",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"0",description:"passenger side door",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"6",description:"driver side door",value:[{charValu:"", valueDesc:"",pricing:""}]},
-      {id:"0",description:"interior lights",value:[{charValu:"", valueDesc:"",pricing:""}]},      
+      { id:"0",
+        description:"vechile identification number",
+        value:[
+          {charValu:"1231", valueDesc:"",pricing:"100",status:''},
+          {charValu:"8888", valueDesc:"",pricing:"600",status:''},
+          {charValu:"9999", valueDesc:"",pricing:"1000",status:''}
+        ]
+      },
+      {
+        id:"2",
+        description:"paint code",
+        value:[
+          {charValu:"a3da", valueDesc:"",pricing:"",status:''},
+          {charValu:"", valueDesc:"",pricing:"",status:''}
+        ]
+      },
+      {
+        id:"3",
+        description:"door",
+        value:[
+          {charValu:"", valueDesc:"",pricing:"",status:''}
+        ]
+      },
+      {
+        id:"4",
+        description:"front options",
+        value:[
+          {charValu:"", valueDesc:"",pricing:"",status:''}
+        ]
+      },
+      {id:"5",description:"interior options",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
+      {id:"0",description:"passenger side door",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
+      {id:"6",description:"driver side door",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},
+      {id:"0",description:"interior lights",value:[{charValu:"", valueDesc:"",pricing:"",status:''}]},      
     ]
+  }
+
+  showVlaues(event,value){
+    console.log(value)
   }
 
   ngOnInit() {
